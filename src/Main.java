7bfb80f9b8f0ef0fc8f31cfda00792957eb3a209
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-10 21:47:51 -0500

    Reference types

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,12 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         byte myAge = 26;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'E';
-        boolean amIAlive = true;
+        Date myDate = new Date();
 
-        System.out.println("Byte: " + myAge);
-        System.out.println("Long: " + viewsCount);
-        System.out.println("Float: " + price);
-        System.out.println("Boolean: " + amIAlive);
-        System.out.println("Char: " + letter);
+        System.out.println(myDate);
+        System.out.println(myDate.getTime());
+        System.out.println("sout shortcut");
     }
 }
