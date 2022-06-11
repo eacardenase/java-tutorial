6b59e9a24ae3799f1ad09941c77c934f27ee5e5e
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-10 21:08:02 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 26;
+        int myGirlfriendAge = myAge - 2;
+        String name = "Edwin";
+        System.out.println("Hi, mi name is " + name + " and I'm " + myAge + " years old.");
+        System.out.println("My girlfriend is " + myGirlfriendAge + " years old.");
+    }
+}
