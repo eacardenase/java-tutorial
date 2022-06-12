a54b4fb442358df8a152ee805c0996f73700f8b3
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-12 18:21:10 -0500

    Math class

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Implicit casting
-        // byte > short > int > long > float > double
-//        short x = 1;
-//        int y = x + 2;
-//        System.out.println(y);
+        int result = (int)Math.ceil(Math.random() * 100);
 
-//        double x = 1.1;
-////        double y = x + 2; // 2.0
-//
-//        // Explicit casting
-//        int y = (int)x + 2;
-
-        String x = "1.1";
-        float y = Float.parseFloat(x) + 2;
-
-        System.out.println(y);
+        System.out.println(result);
     }
 }
