8dd1af58b2ea3affac44ef0d1a5d6df55f394878
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-12 18:37:44 -0500

    Reading input from console

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String result = NumberFormat.getPercentInstance().format(0.75);
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("Your name is " + name + "!");
     }
 }
