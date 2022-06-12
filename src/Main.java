ba60f83e75d46346f4aa6231bb468f59c4970585
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-12 18:06:47 -0500

    Type Casting

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,21 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//        int[][] numbers = new int[3][3];
+        // Implicit casting
+        // byte > short > int > long > float > double
+//        short x = 1;
+//        int y = x + 2;
+//        System.out.println(y);
 
-        int[][] numbers = { { 0, 0, 0}, {0, 0, 0} };
+//        double x = 1.1;
+////        double y = x + 2; // 2.0
+//
+//        // Explicit casting
+//        int y = (int)x + 2;
 
-        numbers[0][0] = 1;
+        String x = "1.1";
+        float y = Float.parseFloat(x) + 2;
 
-        System.out.println(Arrays.deepToString(numbers));
+        System.out.println(y);
     }
 }
