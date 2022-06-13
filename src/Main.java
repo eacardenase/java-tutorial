e480ea20e61f4a914f3cff182d9b2a55f0d106cb
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-12 21:21:07 -0500

    Conditional Statements

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean hasHighIncome = false;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = false;
-        boolean isEligible = !hasCriminalRecord && (hasHighIncome || hasGoodCredit);
+        int temperature = 12;
 
-        System.out.println(isEligible);
+        if (temperature > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink plenty of water");
+        } else if (temperature <= 30 && temperature >= 20) {
+            System.out.println("It's a nice day");
+        } else {
+            System.out.println("It's cold");
+        }
     }
 }
