4f35442eac00637b2122242d6b269887555a5dd6
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-13 20:56:08 -0500

    While Loops example

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int counter = 5;
-        while (counter > 0) {
-            System.out.println("Hello world number " + counter);
-            counter--;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write something. Write 'quit' to exit program.");
+        while (true) {
+            String input = scanner.nextLine().toLowerCase();
+            if (input.equals("quit")) {
+                System.out.println("Bye!");
+                break;
+            }
+            System.out.println("Running...");
         }
     }
 }
