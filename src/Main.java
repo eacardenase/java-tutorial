8b88601f308e79ab6e5bad198b95764663171cf7
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-06-13 21:13:39 -0500

    ForEach Loop

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,14 @@
 import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write something. Write 'quit' to exit program.");
-        while (true) {
-            String input = scanner.nextLine().toLowerCase();
-            if (input.equals("quit")) {
-                System.out.println("Bye!");
-                break;
-            }
-            System.out.println("Running...");
+        String[] fruits = { "Apple", "Orange", "Banana" };
+
+        for (String fruit: fruits) {
+            System.out.println(fruit);
         }
     }
 }
